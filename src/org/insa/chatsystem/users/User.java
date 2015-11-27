50e4d78a581d31f40aa188d832ed52a42fec4e52
Micheau Bastien <micheau0bastien@gmail.com>
2015-11-27 10:36:13 +0100

    Users

---
+++ src/org/insa/chatsystem/users/User.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.insa.chatsystem.users;
+
+import javax.swing.JSplitPane;
+
+/**
+ *
+ * @author Bastien
+ */
+public class User {
+}
