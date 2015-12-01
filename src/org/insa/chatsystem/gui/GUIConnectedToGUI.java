29a3dc7ab58c77650939d08cf97da49d10db9285
LaureBRI <lau-ure@hotmail.fr>
2015-12-01 18:47:41 +0100

    test

--- src/org/insa/chatsystem/gui/GUIConnectedToGUI.java
+++ src/org/insa/chatsystem/gui/GUIConnectedToGUI.java
@@ -6,6 +6,7 @@
 package org.insa.chatsystem.gui;
 
 import java.io.IOException;
+import org.insa.chatsystem.users.UserList;
 
 /**
  *
@@ -13,6 +14,7 @@ import java.io.IOException;
  */
 public interface GUIConnectedToGUI {
 
-    public void logout() throws IOException ;;
+    public void logout() throws IOException ;
+    public UserList fetchUserList();
     
 }
