c7709ad4caa5073f6413834c7baa27f62cfa6939
Micheau Bastien <micheau0bastien@gmail.com>
2015-12-02 12:12:34 +0100

    TCPTest

--- src/org/insa/chatsystem/ni/TCPSender.java
+++ src/org/insa/chatsystem/ni/TCPSender.java
@@ -5,10 +5,37 @@
  */
 package org.insa.chatsystem.ni;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  *
  * @author Bastien
  */
 public class TCPSender extends Thread {
+    private Socket sock;
+    private InputStream input;
+    private OutputStream output;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private int port;
+    private InetAddress address;
     
+    public TCPSender(InetAddress address, int port) throws IOException {
+        this.address = address;
+        this.port = port;
+        sock = new Socket(this.address, this.port);
+        input = sock.getInputStream();
+        output = sock.getOutputStream();
+        reader = new BufferedReader(new InputStreamReader(input));
+        writer = new BufferedWriter(new PrintWriter(new DataOutputStream(output)));
+    }
 }
