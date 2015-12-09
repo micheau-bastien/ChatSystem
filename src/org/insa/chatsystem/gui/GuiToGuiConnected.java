f19f6c51c9a461d9754ce83ebc02961d36abfce7
Micheau Bastien <micheau0bastien@gmail.com>
2015-12-09 21:39:07 +0100

    last Thing

---
+++ src/org/insa/chatsystem/gui/GuiToGuiConnected.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.insa.chatsystem.gui;
+
+import java.net.UnknownHostException;
+import org.insa.chatsystem.users.User;
+
+/**
+ *
+ * @author Bastien
+ */
+public interface GuiToGuiConnected {
+    public void newMessage(User user, String message) throws UnknownHostException;
+}
